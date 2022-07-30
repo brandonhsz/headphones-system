bb915ce6226edddca36a6969ec108e1a07d553c7
brandonhz <brandonhszzz@gmail.com>
2022-07-30 16:59:54 -0500

    Added some visual effects

--- src/components/Nav/NavItem.tsx
+++ src/components/Nav/NavItem.tsx
@@ -9,7 +9,7 @@ const NavItem = ({ link = "#", title }: IProps) => {
   return (
     <>
       <a
-        className="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+        className="text-gray-30 hover:text-white hover:font-bold px-3 py-2 rounded-md text-sm font-medium"
         href={link}
       >
         {title}
--- src/components/Tables/TableItem.tsx
+++ src/components/Tables/TableItem.tsx
@@ -15,31 +15,27 @@ const TableItem = ({ name = '', employeeNumber = '', branch = '', campaign = '',
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
           <div className="flex items-center">
             <div className="ml-3">
-              <p className="text-gray-900 whitespace-no-wrap">
+              <p className="text-gray-900 whitespace-no-wrap text-center">
                 {name}
               </p>
             </div>
           </div>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p className="text-gray-900 whitespace-no-wrap">{employeeNumber}</p>
+          <p className="text-gray-900 whitespace-no-wrap text-center">{employeeNumber}</p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p className="text-gray-900 whitespace-no-wrap">{branch}</p>
+          <p className="text-gray-900 whitespace-no-wrap text-center">{branch}</p>
         </td>
 
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-            <span
-              aria-hidden="true"
-              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-            ></span>
-            <span className="relative">{campaign}</span>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm ">
+          <span className="relative px-3 py-1 font-semibold text-green-900 leading-tight">
+            <p className="relative text-sm text-center bg-green-200 rounded-full py-1 px-2">{campaign}</p>
           </span>
         </td>
 
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p className="text-gray-900 whitespace-no-wrap">{serialNumber}</p>
+          <p className="text-gray-900 whitespace-no-wrap font-black">{serialNumber}</p>
         </td>
 
       </tr>
