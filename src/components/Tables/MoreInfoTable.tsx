9da4dec46ae4dc218ed6f5d7b8baa05f4aae2fae
root <soporte.sistemas1>
2022-08-16 19:53:00 -0500

    Added time to moreInfo

--- src/components/Tables/MoreInfoTable.tsx
+++ src/components/Tables/MoreInfoTable.tsx
@@ -4,7 +4,7 @@ import { useDataStore } from '../../stores/data.store'
 const MoreInfoTable = ({ index }: { index: number }) => {
 
   const { dataFiltered } = useDataStore()
-  const { status } = dataFiltered[index]
+  const { status, createdAt } = dataFiltered[index]
   const { description, type } = dataFiltered[index].headPhone
   console.log(dataFiltered[index]);
   return (
@@ -24,6 +24,10 @@ const MoreInfoTable = ({ index }: { index: number }) => {
 
       <td className='bg-gray-600 rounded-sm p-5'>
         <p className="font-bold whitespace-no-wrap text-center text-sm flex flex-col text-black">Diadema: <span>{status ? 'Activa' : 'Baja'}</span></p>
+      </td>
+
+      <td className='bg-gray-600 rounded-sm p-5'>
+        <p className="font-bold whitespace-no-wrap text-center text-sm flex flex-col text-black">Fecha <span>{createdAt}</span> </p>
       </td>
     </tr>
   )
--- src/interfaces/userinterface.ts
+++ src/interfaces/userinterface.ts
@@ -13,4 +13,5 @@ export interface IUser {
     active: boolean;
     type: string;
   };
+  createdAt?: string;
 } 
