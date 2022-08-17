501f7ba0b3089b63df645f641ff7f3db5e18a182
brandonhz <brandonhszzz@gmail.com>
2022-08-17 14:30:15 -0500

    Fixed dayjs canot call a namespace

--- src/components/Tables/MoreInfoTable.tsx
+++ src/components/Tables/MoreInfoTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDataStore } from '../../stores/data.store'
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 
